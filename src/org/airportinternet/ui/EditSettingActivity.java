7a5b5e6ab7d7223af4d81f348a53cb3e2f8d79a7
Motiejus Jakštys <desired.mta@gmail.com>
2012-03-13 12:31:50 +0000

    Cleanup

--- src/org/airportinternet/ui/EditSettingActivity.java
+++ src/org/airportinternet/ui/EditSettingActivity.java
@@ -4,7 +4,6 @@ import org.airportinternet.R;
 import org.airportinternet.Setting;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
